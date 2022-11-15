a6b7b2ab6ed807b98f416483317788bb52bfe663
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-11-15 10:47:15 +0000

    Update database.php

--- auction/database.php
+++ auction/database.php
@@ -1,6 +1,6 @@
 <?php
     // Create connection
-    $connection = mysqli_connect("localhost","auctadmin","adminpassword","auction"); // Database configuration
+    $connection = mysqli_connect("localhost:8888","auctadmin","adminpassword","auction"); // Database configuration
     // Check connection 
     if (mysqli_connect_errno()) {
         echo "Failed to connect to the MySQL server: " . mysqli_connect_error();
