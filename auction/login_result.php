98bcee86ea8c04fd157736db565f731a9b4aee23
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-11-16 22:35:13 +0000

    Update login_result.php

--- auction/login_result.php
+++ auction/login_result.php
@@ -9,38 +9,42 @@
         // Query 1: check if the user has an account
         $query1 = "SELECT userID FROM User WHERE email = '$email'";
         $check1 = mysqli_query($connection, $query1);
-
+        #$rowcount=mysqli_num_rows($check1);
+        #printf("Result set has %d rows.\n",$rowcount);
         if (mysqli_num_rows($check1) >= 1) {
             // Hash the password
             $hash_pass = md5($password);
             // Query 2: check if the password match the email
             $query2 = "SELECT userID FROM User WHERE email = '$email' AND password = '$hash_pass'";
             $check2 = mysqli_query($connection, $query2);
-
+            #$rowcount2=mysqli_num_rows($check2);
+            #printf("Result set has %d rows.\n",$rowcount2);
             if (mysqli_num_rows($check2) == 1) {
                 // Set session variables
 				session_start();
                 $_SESSION['logged_in'] = true;
-                $user_id = current($connection -> query("SELECT userID FROM User WHERE email = '$email'") -> fetch_assoc());
-                $_SESSION['user_id'] = $user_id;
-                $type = urrent($connection -> query("SELECT userType FROM User WHERE email = '$email'") -> fetch_assoc());
-                $_SESSION['account_type'] = $type;
+		// 等待fix
+                #$user_id = current($connection -> query("SELECT userID FROM User WHERE email = '$email'") -> fetch_assoc());
+                #$_SESSION['user_id'] = $user_id;
+                #$type = urrent($connection -> query("SELECT userType FROM User WHERE email = '$email'") -> fetch_assoc());
+                #$_SESSION['account_type'] = $type;
                 echo '<div class="text-center">You are now logged in! You will be redirected shortly.</div>';
                 // Redirect to index after 5 seconds
                 header("refresh:5;url=index.php");
             } else {
-                echo '<div class="text-center">You have entered a wrong username/password. Please try again. You will be redirected shortly.</div>';
+                echo '<div class="text-center">You have entered a wrong password. Please try again. You will be redirected shortly.</div>';
                 $_SESSION['logged_in'] = false;
                 // Redirect to index after 5 seconds
                 header("refresh:5;url=index.php");
             }
-            
+
         } else {
             echo '<div class="text-center">User not found. Please register a new account. You will be redirected shortly.</div>';
             $_SESSION['logged_in'] = false;
             // Redirect to index after 5 seconds
             header("refresh:5;url=index.php");
         }
+        $connection -> close();
     }
 
 
