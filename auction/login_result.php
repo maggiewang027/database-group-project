01e7fe23e40d5b6be7c96c35e2278345418b1154
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-11-16 09:27:26 +0000

    Update login_result.php

--- auction/login_result.php
+++ auction/login_result.php
@@ -1,6 +1,7 @@
 <?php
     // Connect to the database
     include_once('database.php');
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Obtain the login information
         $email = mysqli_real_escape_string($connection, $_POST['email']);
@@ -8,23 +9,23 @@
         // Query 1: check if the user has an account
         $query1 = "SELECT userID FROM User WHERE email = '$email'";
         $check1 = mysqli_query($connection, $query1);
+
         if (mysqli_num_rows($check1) >= 1) {
             // Hash the password
             $hash_pass = md5($password);
             // Query 2: check if the password match the email
             $query2 = "SELECT userID FROM User WHERE email = '$email' AND password = '$hash_pass'";
             $check2 = mysqli_query($connection, $query2);
+
             if (mysqli_num_rows($check2) == 1) {
-				session_start();
                 // Set session variables
+				session_start();
                 $_SESSION['logged_in'] = true;
                 $user_id = current($connection -> query("SELECT userID FROM User WHERE email = '$email'") -> fetch_assoc());
                 $_SESSION['user_id'] = $user_id;
                 $type = urrent($connection -> query("SELECT userType FROM User WHERE email = '$email'") -> fetch_assoc());
                 $_SESSION['account_type'] = $type;
-                
                 echo '<div class="text-center">You are now logged in! You will be redirected shortly.</div>';
-
                 // Redirect to index after 5 seconds
                 header("refresh:5;url=index.php");
             } else {
@@ -33,6 +34,7 @@
                 // Redirect to index after 5 seconds
                 header("refresh:5;url=index.php");
             }
+            
         } else {
             echo '<div class="text-center">User not found. Please register a new account. You will be redirected shortly.</div>';
             $_SESSION['logged_in'] = false;
