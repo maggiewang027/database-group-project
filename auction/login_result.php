ab487bcbac8fd12361945f914a8a8b7bb366e627
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-11-16 23:13:16 +0000

    Update login_result.php

--- auction/login_result.php
+++ auction/login_result.php
@@ -7,27 +7,26 @@
         $email = mysqli_real_escape_string($connection, $_POST['email']);
         $password = mysqli_real_escape_string($connection, $_POST['password']);
         // Query 1: check if the user has an account
-        $query1 = "SELECT userID FROM User WHERE email = '$email'";
+        $query1 = "SELECT * FROM User WHERE email = '$email'";
         $check1 = mysqli_query($connection, $query1);
-        #$rowcount=mysqli_num_rows($check1);
-        #printf("Result set has %d rows.\n",$rowcount);
+
         if (mysqli_num_rows($check1) >= 1) {
             // Hash the password
             $hash_pass = md5($password);
             // Query 2: check if the password match the email
-            $query2 = "SELECT userID FROM User WHERE email = '$email' AND password = '$hash_pass'";
+            $query2 = "SELECT * FROM User WHERE email = '$email' AND password = '$hash_pass'";
             $check2 = mysqli_query($connection, $query2);
-            #$rowcount2=mysqli_num_rows($check2);
-            #printf("Result set has %d rows.\n",$rowcount2);
+
             if (mysqli_num_rows($check2) == 1) {
+                while ($row = mysqli_fetch_assoc($check2)) {
+                    $user_id = $row['userID'];
+                    $type = $row['userType'];
+                }
                 // Set session variables
 				session_start();
                 $_SESSION['logged_in'] = true;
-		// 等待fix
-                #$user_id = current($connection -> query("SELECT userID FROM User WHERE email = '$email'") -> fetch_assoc());
-                #$_SESSION['user_id'] = $user_id;
-                #$type = urrent($connection -> query("SELECT userType FROM User WHERE email = '$email'") -> fetch_assoc());
-                #$_SESSION['account_type'] = $type;
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['account_type'] = $type;
                 echo '<div class="text-center">You are now logged in! You will be redirected shortly.</div>';
                 // Redirect to index after 5 seconds
                 header("refresh:5;url=index.php");
