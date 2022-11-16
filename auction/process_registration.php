a2bcb3f76c30edc2ce1640ae9cff16354cf33762
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-11-16 09:27:48 +0000

    Update process_registration.php

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -1,4 +1,58 @@
 <?php
+    // Connect to the database
+    include_once('database.php');
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Check if all the information has been entered
+        if (!empty($_POST['name']) && !empty($_POST['accountType']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST["passwordConfirmation"])) {
+            // Set the variables
+            $name = mysqli_real_escape_string($connection, $_POST['name']);
+            $type = mysqli_real_escape_string($connection, $_POST['accountType']);
+            $email = mysqli_real_escape_string($connection, $_POST['email']);
+            $password = mysqli_real_escape_string($connection, $_POST['password']);
+            $pass_confirm = mysqli_real_escape_string($connection, $_POST["passwordConfirmation"]);
+
+            // Check if the password confirmation is same as the original one
+            if ($password == $pass_confirm) {
+            	// Query 1: check if the user already has an account
+                $query = "SELECT email FROM User WHERE email = '$email'";
+                $check = mysqli_query($connection, $query);
+
+                if (mysqli_num_rows($check) == 0) {
+                	// Generate the user id and hash the password
+                	$id = uniqid();
+                    $hash_pass = md5($password);
+                    // Query 2: insert the registration information to the database
+                    $register = 'INSERT INTO `User` (`userID`, `displayName`, `email`, `password`, `userType`) VALUES ('$id', '$name', '$email', '$hash_pass', '$type');';
+                    $result = mysqli_query($connection, $register) or die(mysqli_error($connection));
+                    // Set session variables
+                    session_start();
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['user_id'] = $id;
+                    $_SESSION['account_type'] = $type;
+                    echo '<div class="text-center">You are now registered and logged in! You will be redirected shortly.</div>';
+                    // Redirect to index after 5 seconds
+                    header("refresh:5;url=index.php");
+                } else {
+                    echo '<div class="text-center">The user already existed with the email. Please log in or check your details entered. You will be redirected shortly.</div>';
+                    // Redirect to index after 5 seconds
+                    header("refresh:5;url=index.php");
+                }
+
+            } else {
+                echo '<div class="text-center">The two passwords do not match. Please try again. You will be redirected shortly.</div>';
+                // Redirect to index after 5 seconds
+                header("refresh:5;url=index.php");
+            }
+            
+        } else {
+            echo '<div class="text-center">Please enterer all the required information. You will be redirected shortly.</div>';
+            // Redirect to index after 5 seconds
+            header("refresh:5;url=index.php");
+        }
+    }
+
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
