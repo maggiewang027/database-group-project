b03cfc31dfa8a0f18d9ae8e58ce710ebac9cdcd9
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-12-10 13:45:05 +0000

    Update mywatchlist.php

--- auction/mywatchlist.php
+++ auction/mywatchlist.php
@@ -8,6 +8,15 @@
 
 </div>
 
+<?php
+  if (!isset($_GET['page'])) {
+    $curr_page = 1;
+  }
+  else {
+    $curr_page = $_GET['page'];
+  }
+?>
+
 <div class="container mt-5">
 
 <ul class="list-group">
@@ -48,6 +57,14 @@ $buyerID = $_SESSION['userid'];
      WHERE i.itemID IN (SELECT itemID FROM Watchlist WHERE buyerID = '$buyerID') 
         AND i.itemID = bi.itemID";
     $result = mysqli_query($connection, $query)or die('Error making select users query' . mysql_error());
+      /* For the purposes of pagination, it would also be helpful to know the
+     total number of results that satisfy the above query */
+    $num_results = mysqli_num_rows($result); // TODO: Calculate me for real
+    $results_per_page = 5;
+    $max_page = ceil($num_results / $results_per_page);
+    $page_start = ($curr_page-1) * $results_per_page;  
+    $query .= " LIMIT " . $page_start . ',' . $results_per_page;
+    $result = mysqli_query($connection, $query);
     
   
   // TODO: Loop through results and print them out as list items.
@@ -71,6 +88,68 @@ $buyerID = $_SESSION['userid'];
 ?>
 
 </ul>
+<!-- Pagination for results listings -->
+<nav aria-label="Search results pages" class="mt-5">
+  <ul class="pagination justify-content-center">
+  
+<?php
+
+  // Copy any currently-set GET variables to the URL.
+  $querystring = "";
+  foreach ($_GET as $key => $value) {
+    if ($key != "page") {
+      $querystring .= "$key=$value&amp;";
+    }
+  }
+  
+  $high_page_boost = max(3 - $curr_page, 0);
+  $low_page_boost = max(2 - ($max_page - $curr_page), 0);
+  $low_page = max(1, $curr_page - 2 - $low_page_boost);
+  $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
+  
+  if ($curr_page != 1) {
+    echo('
+    <li class="page-item">
+      <a class="page-link" href="mywatchlist.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
+        <span aria-hidden="true"><i class="fa fa-arrow-left"></i></span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>');
+  }
+    
+  for ($i = $low_page; $i <= $high_page; $i++) {
+    if ($i == $curr_page) {
+      // Highlight the link
+      echo('
+    <li class="page-item active">');
+    }
+    else {
+      // Non-highlighted link
+      echo('
+    <li class="page-item">');
+    }
+    
+    // Do this in any case
+    echo('
+      <a class="page-link" href="mywatchlist.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
+    </li>');
+  }
+  
+  if ($curr_page != $max_page) {
+    echo('
+    <li class="page-item">
+      <a class="page-link" href="mywatchlist.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
+        <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>');
+  }
+
+mysqli_close($connection);
+?>
+
+  </ul>
+</nav>
 </div>
 
 <?php include_once("footer.php")?>
