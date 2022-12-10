023a12e5f4c6b82707452b50ba13f919fc699f7e
liuyiyangsh <115943245+liuyiyangsh@users.noreply.github.com>
2022-12-10 21:27:50 +0800

    Update mybids.php

--- auction/mybids.php
+++ auction/mybids.php
@@ -53,18 +53,6 @@
         print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time);
     }
 
-  // This page is for showing a user the auction listings they've made.
-  // It will be pretty similar to browse.php, except there is no search bar.
-  // This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up their auctions.
-  
-  // TODO: Loop through results and print them out as list items.
 mysqli_close($connection);
 ?>
 
