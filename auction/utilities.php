7615ac4f1567dd193b51e4427604c7860f982d26
Miratong <116718612+Miratong@users.noreply.github.com>
2022-12-11 00:33:36 +0000

    Update utilities.php

--- auction/utilities.php
+++ auction/utilities.php
@@ -23,7 +23,8 @@ function display_time_remaining($interval) {
 
 // print_listing_li:
 // This function prints an HTML <li> element containing an auction listing
-function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time)
+// for user's convenience, we add a new parameter '$bid_belonging' to show whether the latest bid is belong to current buyer
+function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time, $bid_belonging)
 {
   // Truncate long descriptions
   if (strlen($desc) > 250) {
@@ -43,6 +44,7 @@ function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time)
   
   // Calculate time to auction end
   $now = new DateTime();
+  
   if ($now > $end_time) {
     $time_remaining = 'This auction has ended';
   }
@@ -52,11 +54,17 @@ function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time)
     $time_remaining = display_time_remaining($time_to_end) . ' remaining';
   }
   
+  if($bid_belonging == ' (Your bid)'){
+    $color = '#28B463';
+  }else{
+    $color = '#CD5C5C';
+  }
+
   // Print HTML
   echo('
     <li class="list-group-item d-flex justify-content-between">
     <div class="p-2 mr-5"><h5><a href="listing.php?item_id=' . $item_id . '">' . $title . '</a></h5>' . $desc_shortened . '</div>
-    <div class="text-center text-nowrap"><span style="font-size: 1.5em">£' . number_format($price, 2) . '</span><br/>' . $num_bids . $bid . '<br/>' . $time_remaining . '</div>
+    <div class="text-center text-nowrap"><span style="font-size: 1.5em">£' . number_format($price, 2) . '<span style="color:' . $color . '">' . $bid_belonging . '</span>' . '</span><br/>' . $num_bids . $bid . '<br/>' . $time_remaining . '</div>
   </li>'
   );
 }
