0f39aad72ebc1811961a0e989d39b74f62e2aaa1
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-12-11 11:06:48 +0000

    Update process_registration.php

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -7,49 +7,54 @@
 
         // Check if all the information has been entered
         if (!empty($_POST['name']) && !empty($_POST['accountType']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST["passwordConfirmation"])) {
-            // Set the variables
-            $name = mysqli_real_escape_string($connection, $_POST['name']);
-            $type = mysqli_real_escape_string($connection, $_POST['accountType']);
-            $email = mysqli_real_escape_string($connection, $_POST['email']);
-            $password = mysqli_real_escape_string($connection, $_POST['password']);
-            $pass_confirm = mysqli_real_escape_string($connection, $_POST["passwordConfirmation"]);
-            // Query 1: check if the user already has an account
-            $query = "SELECT * FROM User WHERE email = '$email'";
-            $check = mysqli_query($connection, $query);
-
-            // Check if the password confirmation is same as the original one
-            if (mysqli_num_rows($check) == 0) {
-                
-                if ($password == $pass_confirm) {
-                    // Hash the password
-                    $hash_pass = sha1($password);
-                    // Query 2: insert the registration information to the database
-                    $register = "INSERT INTO User (userID, displayName, email, password, userType) VALUES (NULL, '$name', '$email', '$hash_pass', '$type')";
-                    $result = mysqli_query($connection, $register);
-                    // Set session variables
-                    $_SESSION['logged_in'] = true;
-                    $query2 = "SELECT * FROM User WHERE email = '$email'";
-                    $check2 = mysqli_query($connection, $query2);
-                    while ($row = mysqli_fetch_assoc($check2)) {
-                        $_SESSION['userid'] = $row['userID'];
-                    }
-                    $_SESSION['username'] = $name;
-                    $_SESSION['account_type'] = $type;
-                    echo '<div class="text-center">You are now registered and logged in! You will be redirected shortly.</div>';
-                    // Redirect to index after 3 seconds
-                    header("refresh:3;url=index.php");
-                } else {
-                    echo '<div class="text-center">The two passwords do not match. Please try again. You will be redirected shortly.</div>';
-                    // Redirect to index after 3 seconds
-                    header("refresh:3;url=index.php");
-                }
-
-            } else {
-                echo '<div class="text-center">The user already existed with the email. Please log in or check your details entered. You will be redirected shortly.</div>';
+            if(strpos($_POST['email'], '@') == false) {
+                echo '<div class="text-center">Please enter a valid email. You will be redirected shortly.</div>';
                 // Redirect to index after 3 seconds
                 header("refresh:3;url=register.php");
-            }
+            } else {
+                // Set the variables
+                $name = mysqli_real_escape_string($connection, $_POST['name']);
+                $type = mysqli_real_escape_string($connection, $_POST['accountType']);
+                $email = mysqli_real_escape_string($connection, $_POST['email']);
+                $password = mysqli_real_escape_string($connection, $_POST['password']);
+                $pass_confirm = mysqli_real_escape_string($connection, $_POST["passwordConfirmation"]);
+                // Query 1: check if the user already has an account
+                $query = "SELECT * FROM User WHERE email = '$email'";
+                $check = mysqli_query($connection, $query);
 
+                // Check if the password confirmation is same as the original one
+                if (mysqli_num_rows($check) == 0) {
+                    
+                    if ($password == $pass_confirm) {
+                        // Hash the password
+                        $hash_pass = sha1($password);
+                        // Query 2: insert the registration information to the database
+                        $register = "INSERT INTO User (userID, displayName, email, password, userType) VALUES (NULL, '$name', '$email', '$hash_pass', '$type')";
+                        $result = mysqli_query($connection, $register);
+                        // Set session variables
+                        $_SESSION['logged_in'] = true;
+                        $query2 = "SELECT * FROM User WHERE email = '$email'";
+                        $check2 = mysqli_query($connection, $query2);
+                        while ($row = mysqli_fetch_assoc($check2)) {
+                            $_SESSION['userid'] = $row['userID'];
+                        }
+                        $_SESSION['username'] = $name;
+                        $_SESSION['account_type'] = $type;
+                        echo '<div class="text-center">You are now registered and logged in as "'.$_SESSION['username'].'"! You will be redirected shortly.</div>';
+                        // Redirect to index after 3 seconds
+                        header("refresh:3;url=index.php");
+                    } else {
+                        echo '<div class="text-center">The two passwords do not match. Please try again. You will be redirected shortly.</div>';
+                        // Redirect to index after 3 seconds
+                        header("refresh:3;url=index.php");
+                    }
+
+                } else {
+                    echo '<div class="text-center">The user already existed with the email. Please log in or check your details entered. You will be redirected shortly.</div>';
+                    // Redirect to index after 3 seconds
+                    header("refresh:3;url=register.php");
+                }
+            }
         } else {
             echo '<div class="text-center">Please enter all the required information. You will be redirected shortly.</div>';
             // Redirect to index after 3 seconds
